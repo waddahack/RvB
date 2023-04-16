3c69da2aa6da9b43891a98d54c3662af679e79f8
p1804807 <marin.ruelen@etu.univ-lyon1.fr>
2023-04-16 21:12:37 +0200

    DB moved, 2.8 finally rdy ?

--- src/managers/RVBDB.java
+++ src/managers/RVBDB.java
@@ -1,11 +1,13 @@
 package managers;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import rvb.RvB;
@@ -25,10 +27,36 @@ public class RVBDB {
     }
     
     private static void initDB(){
+        String appDataPath = System.getProperty("user.home") + File.separator + "AppData" + File.separator + "Roaming";
+        File appFolder = new File(appDataPath, "RvB");
+        if (!appFolder.exists()) {
+            appFolder.mkdirs();
+        }
+        // Création de la connection
         try {
-            String dataBaseURL = "jdbc:derby:RvBDB";
+            String dataBaseURL = "jdbc:derby:" + appFolder.getAbsolutePath() + File.separator + "RvBDB;create=true";
             connection = DriverManager.getConnection(dataBaseURL);
             System.out.println("Connected to the data base");
+        } catch (SQLException ex) {
+            Logger.getLogger(RVBDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        // Regarde si les tables existent, si non, les créer
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM properties WHERE 1 = 0");
+            ResultSetMetaData meta = rs.getMetaData();
+            int colCount = meta.getColumnCount();
+            // si la requête s'est exécutée sans erreur, la table existe
+        } catch (SQLException e) {
+            // si une exception est levée, la table n'existe pas
+            try {
+                createTables();
+            } catch (SQLException ex) {
+                Logger.getLogger(RVBDB.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        // Fetch les données et les initialises
+        try {
             Statement stmt = connection.createStatement();
             ResultSet prop = getProperties(stmt);
             if(prop.getBoolean("ingame")){
@@ -49,18 +77,49 @@ public class RVBDB {
         }
     }
     
+    private static void createTables() throws SQLException{
+        Statement createProperties = connection.createStatement();
+        String sql = "CREATE TABLE properties (\n" +
+                    "    ingame BOOLEAN DEFAULT false,\n" +
+                    "    progression INTEGER DEFAULT 0,\n" +
+                    "    progressiontuto VARCHAR(20) DEFAULT null,\n" +
+                    "    cheatson BOOLEAN DEFAULT false,\n" +
+                    "    language VARCHAR(5) DEFAULT 'ENG',\n" +
+                    "    bestscoreeasy VARCHAR(10) DEFAULT null,\n" +
+                    "    bestscoremedium VARCHAR(10) DEFAULT null,\n" +
+                    "    bestscorehard VARCHAR(10) DEFAULT null,\n" +
+                    "    bestscorehardcore VARCHAR(10) DEFAULT null\n" +
+                    ")";
+        createProperties.executeUpdate(sql);
+        createProperties.close();
+
+        Statement createGame = connection.createStatement();
+        sql = "CREATE TABLE game (\n" +
+            "    wavenumber INTEGER DEFAULT 1,\n" +
+            "    money INTEGER DEFAULT 0,\n" +
+            "    life INTEGER DEFAULT 0,\n" +
+            "    path VARCHAR(4600) DEFAULT '',\n" +
+            "    difficulty VARCHAR(20) DEFAULT '',\n" +
+            "    towers VARCHAR(10000) DEFAULT null,\n" +
+            "    buffs VARCHAR(5000) DEFAULT '',\n" +
+            "    buffsused VARCHAR(2000) DEFAULT ''\n" +
+            ")";
+        createGame.executeUpdate(sql);
+        createGame.close();
+    }
+    
     private static ResultSet getProperties(Statement stmt) throws SQLException{
         ResultSet prop = stmt.executeQuery("SELECT * FROM properties FETCH FIRST 1 ROWS ONLY");
         if(!prop.next()){
             Statement stmt2 = connection.createStatement();
-            stmt2.executeUpdate("INSERT INTO properties (ingame, progression, progressiontuto, cheatson, language, bestscoreeasy, bestscoremedium, bestscorehard, bestscorehardcore) VALUES (false, 0, null, false, 'ENG', null, null, null)");
+            stmt2.executeUpdate("INSERT INTO properties (ingame, progression, progressiontuto, cheatson, language, bestscoreeasy, bestscoremedium, bestscorehard, bestscorehardcore) VALUES (false, 0, null, false, 'ENG', null, null, null, null)");
             stmt2.close();
             prop = stmt.executeQuery("SELECT * FROM properties FETCH FIRST 1 ROWS ONLY");
             prop.next();
         }
         else{
-            prop.close();
-            checkPropertiesColumns();
+            /*prop.close();
+            checkPropertiesColumns();*/
             prop = stmt.executeQuery("SELECT * FROM properties FETCH FIRST 1 ROWS ONLY");
             prop.next();
         }
@@ -68,7 +127,9 @@ public class RVBDB {
     }
     
     private static void checkPropertiesColumns() throws SQLException{
-        Statement stmt1 = connection.createStatement();
+        // Exemple de quoi faire pour checker si la colonne existe et les créer si non
+        // (à faire si on rajoute des colonnes plus tard, pour pouvoir garder la DB de l'utilisateur intact)
+        /*Statement stmt1 = connection.createStatement();
         ResultSet res = stmt1.executeQuery("SELECT COLUMNNAME FROM SYS.SYSCOLUMNS WHERE COLUMNNAME='BESTSCOREEASY'");
         if(res.next()){
             res.close();
@@ -83,7 +144,7 @@ public class RVBDB {
         stmt2.executeUpdate("ALTER TABLE properties ADD COLUMN bestscoremedium VARCHAR(10) DEFAULT null");
         stmt2.executeUpdate("ALTER TABLE properties ADD COLUMN bestscorehard VARCHAR(10) DEFAULT null");
         stmt2.executeUpdate("ALTER TABLE properties ADD COLUMN bestscorehardcore VARCHAR(10) DEFAULT null");
-        stmt2.close();
+        stmt2.close();*/
     }
     
     public static boolean saveGame(int waveNumber, int money, int life, String pathString, String difficulty, String towers, String buffs, String buffsUsed) throws SQLException{
